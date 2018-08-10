d0aaec2cc842c71775a93f87b011d53a8c4afcd0
Mark Williams <thyme676@users.noreply.github.com>
2018-08-10 10:54:11 -0700

    Create mirror.sh
    This will download the latest kernel into a directory, meant to be web accessible.

---
+++ mirror.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+version 1.6
+kernels='./kernels'
+
+aria2c -q -x 16 --allow-overwrite=true  http://kernel.ubuntu.com/~kernel-ppa/mainline/ ;
+
+echo -e "Latest kernel is:"  ;
+
+chmod 644 index.html
+#latest=`tail -n 5 index.html | grep href= | cut -c 81-89 `
+latest=`tail -n 5 index.html | grep href= | cut -d"/"  -f5 | cut -c 3-`
+echo $latest
+echo $latest > latest
+
+
+aria2c -q -x 16 --allow-overwrite=true  http://kernel.ubuntu.com/~kernel-ppa/mainline/${latest}/ ;
+
+all=`cat index.html | grep headers | grep href= | sed -n 1p | cut -d">" -f2 | cut -d"<" -f1`
+headers=`cat index.html | grep headers | grep href= | sed -n 2p | cut -d">" -f2 | cut -d"<" -f1`
+image=`cat index.html | grep image | grep href= | sed -n 1p | cut -d">" -f2 | cut -d"<" -f1`
+modules=`cat index.html | grep modules | grep href= | sed -n 1p | cut -d">" -f2 | cut -d"<" -f1`
+
+echo -e $all "\n" $headers "\n" $image
+
+cd ${kernels};
+pwd
+sleep 10;
+aria2c  -x 16 http://kernel.ubuntu.com/~kernel-ppa/mainline/${latest}/${all} --auto-file-renaming=false
+aria2c  -x 16 http://kernel.ubuntu.com/~kernel-ppa/mainline/${latest}/${headers} --auto-file-renaming=false
+aria2c  -x 16 http://kernel.ubuntu.com/~kernel-ppa/mainline/${latest}/${image} --auto-file-renaming=false
+aria2c  -x 16 http://kernel.ubuntu.com/~kernel-ppa/mainline/${latest}/${modules} --auto-file-renaming=false
+
+chmod 744 ${all} ${headers} ${image} ${modules} 
